3cff5464b7e6b074c4376336ae294cc3c530b214
cygauss <117000961+cygauss@users.noreply.github.com>
2023-04-16 18:49:23 +0800

    Update 2.sh

--- newgen/2.sh
+++ newgen/2.sh
@@ -2,7 +2,7 @@ mount /dev/nvme2n1p2 /boot
 emerge-webrsync
 emerge --update --deep --newuse @world
 emerge cpuid2cpuflags
-echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
+echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use
 echo "Asia/Chongqing" > /etc/timezone
 emerge --config timezone-data
 echo "en_SG.UTF-8 UTF-8" > /etc/locale.gen
